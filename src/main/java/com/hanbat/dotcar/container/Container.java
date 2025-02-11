00ef937aee6a8aff6906f9ec4400a2cc8e7d4796
서민재 <hey_minj@naver.com>
2025-02-12 01:37:03 +0900

    컨테이너 DB 1차 생성

--- src/main/java/com/hanbat/dotcar/container/Container.java
+++ src/main/java/com/hanbat/dotcar/container/Container.java
@@ -1,4 +1,40 @@
 package com.hanbat.dotcar.container;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Builder
 public class Container {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String containerID; //컨테이너 아이디
+
+    private String containerName; // 컨테이너 이름
+
+    @Column(nullable = false)
+    private String OS; //운영체제
+
+    private String version; //버전
+
+    private Date createdAt; //컨테이너 생성 시간
+    private String status; // 컨테이너 상태
+    private int hostPort;  // 컨테이너가 매핑된 호스트 포트 번호
+
+    private String madeBy; //생성한 사람의 이메일
+
+
+
 }
--- src/main/java/com/hanbat/dotcar/container/CreateContainerRequestDto.java
+++ src/main/java/com/hanbat/dotcar/container/CreateContainerRequestDto.java
@@ -12,4 +12,5 @@ import lombok.NoArgsConstructor;
 public class CreateContainerRequestDto {
     private String OS;
     private String version;
+    private String userEmail;
 }
