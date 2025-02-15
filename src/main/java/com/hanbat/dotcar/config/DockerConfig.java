d1172639d9914c8cbf1b32d659b05a7fb5d33ec2
서민재 <hey_minj@naver.com>
2025-02-15 16:20:52 +0900

    dockerconfig

--- src/main/java/com/hanbat/dotcar/config/DockerConfig.java
+++ src/main/java/com/hanbat/dotcar/config/DockerConfig.java
@@ -27,16 +27,18 @@ public class DockerConfig {
     @Bean
     public DockerHttpClient dockerHttpClient(DockerClientConfig dockerClientConfig) {
         return new ZerodepDockerHttpClient.Builder()
-                .dockerHost(dockerClientConfig.getDockerHost())
-                .sslConfig(dockerClientConfig.getSSLConfig())
-                .maxConnections(100)
-                .connectionTimeout(Duration.ofSeconds(30))
-                .responseTimeout(Duration.ofSeconds(45))
+                .dockerHost(dockerClientConfig.getDockerHost())          // DockerClientConfig에서 Docker 호스트 정보 가져오기
+                .sslConfig(dockerClientConfig.getSSLConfig())              // SSL 구성 (TLS 인증서 등)
+                .maxConnections(100)                           // 최대 연결 수 설정
+                .connectionTimeout(Duration.ofSeconds(30))     // 연결 타임아웃 설정
+                .responseTimeout(Duration.ofSeconds(45))       // 응답 타임아웃 설정
                 .build();
     }
 
     @Bean
+    // DockerClient 인스턴스 생성 -> DockerClientConfig와 DockerHttpClient를 결합하여 Docker 데몬에 명령을 전달할 수 있는 DockerClient 객체를 생성
     public DockerClient dockerClient(DockerClientConfig dockerClientConfig, DockerHttpClient dockerHttpClient) {
         return DockerClientImpl.getInstance(dockerClientConfig, dockerHttpClient);
     }
+    /**************************/
 }
