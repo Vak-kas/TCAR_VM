f6d440d7ac1775e453aaee6edd1407e39e42eeed
서민재 <hey_minj@naver.com>
2025-02-23 18:25:45 +0900

    컨테이너 접속 초기 설정

---
+++ src/main/java/com/hanbat/dotcar/config/PreSignedUrlConfig.java
@@ -0,0 +1,15 @@
+package com.hanbat.dotcar.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PreSignedUrlConfig {
+
+    @Value("${pre-signed.url}")
+    private String preSignedUrl;
+
+    public String getPreSignedUrl() {
+        return preSignedUrl;
+    }
+}
