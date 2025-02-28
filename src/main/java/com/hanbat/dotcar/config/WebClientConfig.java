6a16923288dfd7de115fa50adc078e04eba22ff0
서민재 <hey_minj@naver.com>
2025-02-28 14:04:18 +0900

    /제거

--- src/main/java/com/hanbat/dotcar/config/WebClientConfig.java
+++ src/main/java/com/hanbat/dotcar/config/WebClientConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
-    private static final String BACKEND_SERVER_IP = "http://192.168.1.10:8080/";
+    private static final String BACKEND_SERVER_IP = "http://192.168.1.10:8080";
 
     @Bean
     public WebClient webClient() {
