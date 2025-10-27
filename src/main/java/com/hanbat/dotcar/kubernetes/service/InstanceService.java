83e234c181a33ec7a1ec22ee3c806ed7b0db9008
서민재 <hey_minj@naver.com>
2025-10-27 23:08:10 +0900

    feat : pod 접근 권한 부여

--- src/main/java/com/hanbat/dotcar/kubernetes/service/InstanceService.java
+++ src/main/java/com/hanbat/dotcar/kubernetes/service/InstanceService.java
@@ -1,5 +1,6 @@
 package com.hanbat.dotcar.kubernetes.service;
 
+import com.hanbat.dotcar.access.service.AccessAuthorityService;
 import com.hanbat.dotcar.kubernetes.domain.Pod;
 import com.hanbat.dotcar.kubernetes.domain.PodStatus;
 import com.hanbat.dotcar.kubernetes.dto.DeletePodRequestDto;
@@ -26,6 +27,7 @@ public class InstanceService {
     private final IngressService ingressService;
     private final PodService podService;
     private final ServiceService serviceService;
+    private final AccessAuthorityService accessAuthorityService;
 
     public PodInfoDto createInstance(CreatePodRequestDto requestDto) throws ApiException{
         String os = requestDto.getOs();
@@ -69,6 +71,8 @@ public class InstanceService {
                 .build();
 
         podRepository.save(dbPod);
+
+        accessAuthorityService.setAuthority(dbPod, userEmail);
 
 
         PodInfoDto podInfoDto = PodInfoDto.builder()
